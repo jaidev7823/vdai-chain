a7cbe68cb7d3abbb7829db654a64b92f12459ca5
jai mishra <jaidev7823@gmail.com>
2025-10-27 01:47:48 +0530

    added first draw workflow

--- app/workflow_agent.py
+++ app/workflow_agent.py
@@ -5,12 +5,21 @@ from llama_index.core.workflow import (
     step,
 )
 import asyncio
+from llama_index.utils.workflow import draw_all_possible_flows
+
 
 class MyWorkflow(Workflow):
     @step
     async def my_step(self, ev: StartEvent) -> StopEvent:
         return StopEvent(result="Hello, world!")
 
+draw_all_possible_flows(
+    MyWorkflow,
+    filename="basic_workflow.html",
+    # Optional, can limit long event names in your workflow
+    # Can help with readability
+    # max_label_length=10,
+)
 
 async def main():
     w = MyWorkflow(timeout=10, verbose=False)
