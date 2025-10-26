95aaecac5b6b748c129d53ba8ec812d3498e78a2
jai mishra <jaidev7823@gmail.com>
2025-10-27 01:53:34 +0530

    added more steps in worflow

--- app/workflow_agent.py
+++ app/workflow_agent.py
@@ -3,15 +3,32 @@ from llama_index.core.workflow import (
     StopEvent,
     Workflow,
     step,
+    Event,
 )
 import asyncio
 from llama_index.utils.workflow import draw_all_possible_flows
 
+class FirstEvent(Event):
+    first_output: str
+
+class SecondEvent(Event):
+    second_output: str
 
 class MyWorkflow(Workflow):
     @step
-    async def my_step(self, ev: StartEvent) -> StopEvent:
-        return StopEvent(result="Hello, world!")
+    async def step_one(self, ev: StartEvent) -> FirstEvent:
+        print(ev.first_input)
+        return FirstEvent(first_output="First step complete.")
+
+    @step
+    async def step_two(self, ev: FirstEvent) -> SecondEvent:
+        print(ev.first_output)
+        return SecondEvent(second_output="Second step complete.")
+
+    @step
+    async def step_three(self, ev: SecondEvent) -> StopEvent:
+        print(ev.second_output)
+        return StopEvent(result="Workflow complete.")
 
 draw_all_possible_flows(
     MyWorkflow,
@@ -23,7 +40,7 @@ draw_all_possible_flows(
 
 async def main():
     w = MyWorkflow(timeout=10, verbose=False)
-    result = await w.run()
+    result = await w.run(first_input="Start the workflow.")
     print(result)
 
 asyncio.run(main())
