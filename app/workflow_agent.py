8744d0475bb52ddef30f2903dacedf3f168b2051
jai mishra <jaidev7823@gmail.com>
2025-10-27 13:43:22 +0530

    subclassing another workflow

--- app/workflow_agent.py
+++ app/workflow_agent.py
@@ -4,97 +4,59 @@ from llama_index.core.workflow import (
     Workflow,
     step,
     Event,
-    Context,
 )
 import asyncio
 from llama_index.utils.workflow import draw_all_possible_flows
-import random
-from pydantic import BaseModel, Field, field_validator, field_serializer
-from typing import Union
-from llama_index.llms.google_genai import GoogleGenAI
 from dotenv import load_dotenv
 import os
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
-class StepAEvent(Event):
+class Step2Event(Event):
     query: str
 
 
-class StepBEvent(Event):
+class Step3Event(Event):
     query: str
 
-
-class StepCEvent(Event):
+class Step2BEvent(Event):
     query: str
 
-
-class StepACompleteEvent(Event):
-    result: str
-
-
-class StepBCompleteEvent(Event):
-    result: str
-
-
-class StepCCompleteEvent(Event):
-    result: str
-
-
-class ConcurrentFlow(Workflow):
+class MainWorkflow(Workflow):
     @step
-    async def start(
-        self, ctx: Context, ev: StartEvent
-    ) -> StepAEvent | StepBEvent | StepCEvent | None:
-        ctx.send_event(StepAEvent(query="Query 1"))
-        ctx.send_event(StepBEvent(query="Query 2"))
-        ctx.send_event(StepCEvent(query="Query 3"))
+    async def start(self, ev: StartEvent) -> Step2Event:
+        print("Starting up")
+        return Step2Event(query=ev.query)
 
     @step
-    async def step_a(self, ctx: Context, ev: StepAEvent) -> StepACompleteEvent:
-        print("Doing something A-ish")
-        return StepACompleteEvent(result=ev.query)
+    async def step_two(self, ev: Step2Event) -> Step3Event:
+        print("Sending an email")
+        return Step3Event(query=ev.query)
 
     @step
-    async def step_b(self, ctx: Context, ev: StepBEvent) -> StepBCompleteEvent:
-        print("Doing something B-ish")
-        return StepBCompleteEvent(result=ev.query)
+    async def step_three(self, ev: Step3Event) -> StopEvent:
+        print("Finishing up")
+        return StopEvent(result=ev.query)
+
+class CustomWorkflow(MainWorkflow):
+    @step
+    async def step_two(self, ev: Step2Event) -> Step2BEvent:
+        print("Sending an email")
+        return Step2BEvent(query=ev.query)
 
     @step
-    async def step_c(self, ctx: Context, ev: StepCEvent) -> StepCCompleteEvent:
-        print("Doing something C-ish")
-        return StepCCompleteEvent(result=ev.query)
-
-    @step
-    async def step_three(
-        self,
-        ctx: Context,
-        ev: StepACompleteEvent | StepBCompleteEvent | StepCCompleteEvent,
-    ) -> StopEvent:
-        print("Received event ", ev.result)
-
-        # wait until we receive 3 events
-        if (
-            ctx.collect_events(
-                ev,
-                [StepCCompleteEvent, StepACompleteEvent, StepBCompleteEvent],
-            )
-            is None
-        ):
-            return None
-
-        # do something with all 3 results together
-        return StopEvent(result="Done")
-
+    async def step_two_b(self, ev: Step2BEvent) -> Step3Event:
+        print("Also sending a text message")
+        return Step3Event(query=ev.query)
 
 async def main():
-    w = ConcurrentFlow(timeout=30, verbose=True)
-    handler = w.run(first_input="Start the workflow.")
+    w = MainWorkflow(timeout=30, verbose=True)
+    handler = w.run(query="Start the workflow.")
 
     final_result = await handler
     print("Final result", final_result)
 
-    draw_all_possible_flows(ConcurrentFlow, filename="streaming_workflow.html")
+    draw_all_possible_flows(MainWorkflow, filename="basic_workflow.html")
 
 
 if __name__ == "__main__":
