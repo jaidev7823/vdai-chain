1b71738da2b6cda7ffebaf32ade790478f708260
jai mishra <jaidev7823@gmail.com>
2025-10-27 13:34:14 +0530

    Concurrent executing workflow

--- app/workflow_agent.py
+++ app/workflow_agent.py
@@ -11,63 +11,91 @@ from llama_index.utils.workflow import draw_all_possible_flows
 import random
 from pydantic import BaseModel, Field, field_validator, field_serializer
 from typing import Union
+from llama_index.llms.google_genai import GoogleGenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
 
-# This is a random object that we want to use in our state
-class MyRandomObject:
-    def __init__(self, name: str = "default"):
-        self.name = name
+class StepAEvent(Event):
+    query: str
 
-# This is our state model
-# NOTE: all fields must have defaults
-class MyState(BaseModel):
-    model_config = {"arbitrary_types_allowed": True}
-    my_obj: MyRandomObject = Field(default_factory=MyRandomObject)
-    some_key: str = Field(default="some_value")
 
-    # This is optional, but can be useful if you want to control the serialization of your state!
+class StepBEvent(Event):
+    query: str
 
-    @field_serializer("my_obj", when_used="always")
-    def serialize_my_obj(self, my_obj: MyRandomObject) -> str:
-        return my_obj.name
 
-    @field_validator("my_obj", mode="before")
-    @classmethod
-    def deserialize_my_obj(
-        cls, v: Union[str, MyRandomObject]
-    ) -> MyRandomObject:
-        if isinstance(v, MyRandomObject):
-            return v
-        if isinstance(v, str):
-            return MyRandomObject(v)
+class StepCEvent(Event):
+    query: str
 
-        raise ValueError(f"Invalid type for my_obj: {type(v)}")
 
-class MyStatefulFlow(Workflow):
+class StepACompleteEvent(Event):
+    result: str
+
+
+class StepBCompleteEvent(Event):
+    result: str
+
+
+class StepCCompleteEvent(Event):
+    result: str
+
+
+class ConcurrentFlow(Workflow):
     @step
-    async def start(self, ctx: Context[MyState], ev: StartEvent) -> StopEvent:
-        # Allows for atomic state updates
-        async with ctx.store.edit_state() as state:
-            state.my_obj.name = "new_name"
+    async def start(
+        self, ctx: Context, ev: StartEvent
+    ) -> StepAEvent | StepBEvent | StepCEvent | None:
+        ctx.send_event(StepAEvent(query="Query 1"))
+        ctx.send_event(StepBEvent(query="Query 2"))
+        ctx.send_event(StepCEvent(query="Query 3"))
 
-        # Can also access fields directly if needed
-        name = await ctx.store.get("my_obj.name")
+    @step
+    async def step_a(self, ctx: Context, ev: StepAEvent) -> StepACompleteEvent:
+        print("Doing something A-ish")
+        return StepACompleteEvent(result=ev.query)
 
-        return StopEvent(result="Done!")
+    @step
+    async def step_b(self, ctx: Context, ev: StepBEvent) -> StepBCompleteEvent:
+        print("Doing something B-ish")
+        return StepBCompleteEvent(result=ev.query)
 
-draw_all_possible_flows(
-    MyStatefulFlow,
-    filename="basic_workflow.html",
-    # Optional, can limit long event names in your workflow
-    # Can help with readability
-    # max_label_length=10,
-)
+    @step
+    async def step_c(self, ctx: Context, ev: StepCEvent) -> StepCCompleteEvent:
+        print("Doing something C-ish")
+        return StepCCompleteEvent(result=ev.query)
+
+    @step
+    async def step_three(
+        self,
+        ctx: Context,
+        ev: StepACompleteEvent | StepBCompleteEvent | StepCCompleteEvent,
+    ) -> StopEvent:
+        print("Received event ", ev.result)
+
+        # wait until we receive 3 events
+        if (
+            ctx.collect_events(
+                ev,
+                [StepCCompleteEvent, StepACompleteEvent, StepBCompleteEvent],
+            )
+            is None
+        ):
+            return None
+
+        # do something with all 3 results together
+        return StopEvent(result="Done")
+
 
 async def main():
-    w = MyStatefulFlow(timeout=10, verbose=False)
+    w = ConcurrentFlow(timeout=30, verbose=True)
+    handler = w.run(first_input="Start the workflow.")
 
-    ctx = Context(w)
-    result = await w.run(ctx=ctx)
-    state = await ctx.store.get_state()
-    print(state)
+    final_result = await handler
+    print("Final result", final_result)
 
-asyncio.run(main())
+    draw_all_possible_flows(ConcurrentFlow, filename="streaming_workflow.html")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
